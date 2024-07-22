dbfd77b978a8ed387c9b009ea4c12f5c24e9477d
Rolln <rollnthndr@gmail.com>
2024-07-22 11:31:23 -0600

    Added radio.modulation enum

---
+++ library/mission/radio.lua
@@ -0,0 +1,11 @@
+---@meta
+
+---The radio enumerator has a single table that defines the radio modulation used to transmit. 
+---@class radio
+radio = {}
+
+---@enum radio.modulation
+radio.modulation = {
+    AM = 0,
+    FM = 1
+  }
