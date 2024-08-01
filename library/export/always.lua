0e8da8e2b3b0794b03cc51e905d0759d35371a77
Rolln <rollnthndr@gmail.com>
2024-07-31 21:52:59 -0600

    Added export 'is allowed' functions.

--- library/export/always.lua
+++ library/export/always.lua
@@ -1,5 +1,20 @@
 ---@meta
 
+---Returns the value of server.advanced.allow_object_export.
+---___
+---@return boolean isAllowed Is object export allowed.
+function LoIsObjectExportAllowed() end
+
+---Returns the value of server.advanced.allow_sensor_export.
+---___
+---@return boolean isAllowed Is sensor export allowed.
+function LoIsSensorExportAllowed() end
+
+---Returns the value of  server.advanced.allow_ownship_export
+---___
+---@return boolean isAllowed Is ownership export allowed.
+function LoIsOwnshipExportAllowed() end
+
 function LoGetPilotName() end
 function LoGetAltitude() end
 function LoGetNameByType() end
